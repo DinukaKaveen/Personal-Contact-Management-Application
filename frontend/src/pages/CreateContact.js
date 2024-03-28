21fb6b373271b16367a42d2d36e7a242df8dbf41
Dinuka Kaveen Dominguhewa <dinukadominguhewa@gmail.com>
2024-03-28 11:20:53 +0530

    create contact form

--- frontend/src/pages/CreateContact.js
+++ frontend/src/pages/CreateContact.js
@@ -1,9 +1,114 @@
-import React from 'react'
+import React, { useState } from "react";
+import axios from "axios";
 
 function CreateContact() {
+  const [message, setMessage] = useState("");
+  const [name, setName] = useState("");
+  const [email, setEmail] = useState("");
+  const [phoneNo, setPhoneNo] = useState("");
+  const [note, setNote] = useState("");
+
+  const Submit = async (e) => {
+    e.preventDefault();
+    const contact = { name, email, phoneNo, note };
+
+    await axios
+      .post("http://localhost:8000/api/create_contact", contact)
+      .then((res) => {
+        if (res.data.success) {
+          setMessage(res.data.message);
+        } else {
+          setMessage(res.data.message);
+        }
+      })
+      .catch((err) => {
+        console.log(err);
+      });
+  };
+
   return (
-    <div>CreateContact</div>
-  )
+    <div>
+      <form className="max-w-sm mx-auto" onSubmit={(e) => Submit(e)}>
+        {message && <p>{message}</p>}
+        <br />
+
+        <div className="mb-5">
+          <label
+            htmlFor="name"
+            className="block mb-2 text-sm font-medium text-gray-900 dark:text-white"
+          >
+            Name
+          </label>
+          <input
+            type="text"
+            id="name"
+            name="name"
+            onChange={(e) => setName(e.target.value)}
+            value={name}
+            className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
+            required
+          />
+        </div>
+        <div className="mb-5">
+          <label
+            htmlFor="email"
+            className="block mb-2 text-sm font-medium text-gray-900 dark:text-white"
+          >
+            Email
+          </label>
+          <input
+            type="email"
+            id="email"
+            name="email"
+            onChange={(e) => setEmail(e.target.value)}
+            value={email}
+            className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
+            required
+          />
+        </div>
+        <div className="mb-5">
+          <label
+            htmlFor="phone"
+            className="block mb-2 text-sm font-medium text-gray-900 dark:text-white"
+          >
+            Phone Number
+          </label>
+          <input
+            type="text"
+            id="phone"
+            name="phone"
+            onChange={(e) => setPhoneNo(e.target.value)}
+            value={phoneNo}
+            className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
+            required
+          />
+        </div>
+        <div className="mb-5">
+          <label
+            htmlFor="note"
+            className="block mb-2 text-sm font-medium text-gray-900 dark:text-white"
+          >
+            Note
+          </label>
+          <input
+            type="text"
+            id="note"
+            name="note"
+            onChange={(e) => setNote(e.target.value)}
+            value={note}
+            className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
+            required
+          />
+        </div>
+        <button
+          type="submit"
+          className="text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm w-full sm:w-auto px-5 py-2.5 text-center dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800"
+        >
+          Submit
+        </button>
+      </form>
+    </div>
+  );
 }
 
-export default CreateContact
+export default CreateContact;
