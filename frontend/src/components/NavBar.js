7e689f221b9a29f263ebdb312d3fca3c2d7c22a3
Dinuka Kaveen Dominguhewa <dinukadominguhewa@gmail.com>
2024-03-28 10:57:36 +0530

    Create NavBar.js

---
+++ frontend/src/components/NavBar.js
@@ -0,0 +1,36 @@
+import React from "react";
+
+function NavBar() {
+  return (
+    <div>
+      <nav className="bg-gray-50 dark:bg-gray-700">
+        <div className="max-w-screen-xl px-4 py-3 mx-auto">
+          <div className="flex items-center">
+            <ul className="flex flex-row font-medium mt-0 space-x-8 rtl:space-x-reverse text-sm">
+              <li>
+                <Link
+                  to={"/"}
+                  className="text-gray-900 dark:text-white hover:underline"
+                  aria-current="page"
+                >
+                  Contacts
+                </Link>
+              </li>
+              <li>
+                <Link
+                  to={"/contact/create"}
+                  className="text-gray-900 dark:text-white hover:underline"
+                >
+                  Create Contact
+                </Link>
+              </li>
+            </ul>
+          </div>
+        </div>
+      </nav>
+      <br></br>
+    </div>
+  );
+}
+
+export default NavBar;
