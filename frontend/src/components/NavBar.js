199fa033e707af32984c709a9a044bf9b5d10fae
Dinuka Kaveen Dominguhewa <dinukadominguhewa@gmail.com>
2024-03-28 11:07:05 +0530

    Update NavBar.js

--- frontend/src/components/NavBar.js
+++ frontend/src/components/NavBar.js
@@ -10,7 +10,7 @@ function NavBar() {
             <ul className="flex flex-row font-medium mt-0 space-x-8 rtl:space-x-reverse text-sm">
               <li>
                 <Link
-                  to={"/"}
+                  to={"/contacts"}
                   className="text-gray-900 dark:text-white hover:underline"
                   aria-current="page"
                 >
