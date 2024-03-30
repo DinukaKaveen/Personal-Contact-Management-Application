bba00a49b39fd963ade078cf783ef31d6957a3a0
Dinuka Kaveen Dominguhewa <94813111+DinukaKaveen@users.noreply.github.com>
2024-03-30 16:52:27 +0530

    Add files via upload

---
+++ backend/routes/api.php
@@ -0,0 +1,14 @@
+<?php
+
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\ContactController;
+
+Route::get('/user', function (Request $request) {
+    return $request->user();
+})->middleware('auth:sanctum');
+
+
+Route::get('contacts', [ContactController::class, 'get_contacts']);
+Route::post('create_contact', [ContactController::class, 'create_contact']);
+Route::delete('delete_contact/{id}', [ContactController::class, 'delete_contact']);
